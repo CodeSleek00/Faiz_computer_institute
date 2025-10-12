1667c03da8c7e7450fe1ba15763bfd07c7dbefdb
CodeSleek00 <codesleekstudio@gmail.com>
2025-10-12 15:58:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css?v=1.9">
+    <link rel="stylesheet" href="style.css?v=1.8">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="icon" type="image/png" href="logo.png">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <title>Faiz Computer Institute</title>
+    <style>
+        body {
+    font-family: 'Poppins', sans-serif;
+    margin: 0;
+    padding: 0;
+}
+.head {
+    background-color: #000000;
+    padding: 5px 0;
+}
+.head1 {
+    list-style: none;
+    display: flex;
+    justify-content: left;
+    gap: 30px;
+}
+.head1 a {
+    text-decoration: none;
+    color: white;
+}
+.head1 a:hover {
+    color: black;
+    background-color: rgb(255, 255, 255);
+    transition: 0.3s ease-in-out;
+    padding: 5px 10px;
+    border-radius: 5px;
+}
+    </style>
 </head>
 <body>
     <header>
