e8f80a7e01f6b726eb57fd989f8e702448cfe7dc
CodeSleek00 <codesleekstudio@gmail.com>
2025-10-19 20:06:15 +0530

    c

--- test1.php
+++ test1.php
@@ -26,7 +26,7 @@ body {
 .course-2-section-container::-webkit-scrollbar { display: none; }
 
 .course-2-section {
-    flex: 0 0 33.3%;
+    flex: 0 0 420px;
     scroll-snap-align: start;
     background: white;
     border-radius: 16px;
