c4561115f6979f4c31381194d6be63b5ec9c0435
CodeSleek00 <codesleekstudio@gmail.com>
2025-10-22 13:14:56 +0530

    Update university.php

--- university/university.php
+++ university/university.php
@@ -80,8 +80,8 @@ function showCourseSection($conn, $category, $limit = 4) {
             <div class="dropdown" id="dropdownMenu">
                 <div class="dropdown-column">
                     <h4>Explore </h4>
-                   <a href="#"><i class="fas fa-code"></i> Computer Courses</a>
-                <a href="#"><i class="fas fa-paint-brush"></i> University Courses</a>
+                    <a href="../index.php" style="text-decoration: none;"><i class="fas fa-code"></i> Computer Courses</a>
+                <a href="university.php"><i class="fas fa-paint-brush"></i> University Courses</a>
                 <a href="#"><i class="fas fa-chart-bar"></i> Schooling</a>
                 <a href="#"><i class="fas fa-shield-alt"></i> Free Courses</a>
                 </div>
