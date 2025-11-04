be243a754e1701a25a1eecb5259d950a3f7125f7
CodeSleek00 <codesleekstudio@gmail.com>
2025-11-04 22:57:13 +0530

    difference

--- o-level/index.php
+++ o-level/index.php
@@ -6,7 +6,7 @@ $courses = $conn->query("SELECT * FROM single_courses ORDER BY id DESC");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="olevel.css?v=1.7">
+    <link rel="stylesheet" href="olevel.css?v=1.71">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="icon" type="image/png" href="images/logo.png">
