e6d756d280a8afe6b07b77de2dfef5e95e5df17e
CodeSleek00 <codesleekstudio@gmail.com>
2025-11-14 09:26:21 +0530

    Update index.php

--- o-level/index.php
+++ o-level/index.php
@@ -37,7 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['payment_confirmed']))
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="olevel.css?v=1.71">
+    <link rel="stylesheet" href="olevel.css?v=1.72">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="icon" type="image/png" href="images/logo.png">
